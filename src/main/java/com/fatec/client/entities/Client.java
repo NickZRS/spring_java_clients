997bda5cc02bbe94e994e35228303dbd245b69cc
beamatos03 <bea.matos1978@gmail.com>
2023-05-30 11:09:00 -0300

    commit_final

--- src/main/java/com/fatec/client/entities/Client.java
+++ src/main/java/com/fatec/client/entities/Client.java
@@ -1,17 +1,32 @@
 package com.fatec.client.entities;
 
-public class Client {
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "TBL_CLIENT")
+
+
+
+public class Client implements Serializable {
+    public Client() {
+    }
+
     public Client(int id, String name, String email) {
         this.id = id;
         this.name = name;
         this.email = email;
     }
 
-    public int getId() {
+    public Integer getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Integer id) {
         this.id = id;
     }
 
@@ -31,7 +46,41 @@ public class Client {
         this.email = email;
     }
 
-    private int id;
+    @Id
+    private Integer id;
+
     private String name;
+
+    @Column(unique = true)
     private String email;
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Client other = (Client) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+
+
+
+    
 }
