203fd8ff565fe1a9761d5ebdee15cd0275c65f71
iericksouza <erickrucke1@gmail.com>
2023-06-06 08:55:08 -0300

    commit_add

--- src/main/java/com/fatec/client/resources/ClientController.java
+++ src/main/java/com/fatec/client/resources/ClientController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,16 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
 import com.fatec.client.entities.Client;
 import com.fatec.client.services.ClientService;
 
+@CrossOrigin
 @RestController
 public class ClientController {
 
     @Autowired
     private ClientService clientService;
 
-    
-
     @GetMapping("clients/{id}")
-    public Client getClient(@PathVariable int id){
+    public Client getClient(@PathVariable int id) {
         return clientService.getClientById(id);
     }
 
